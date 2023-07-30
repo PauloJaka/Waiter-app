8d341e3b4c218a6f3b855026434c90c873ae29da
PauloJaka <zpaulogamer@gmail.com>
2023-07-29 23:27:38 -0300

    arquivos moongose para salvar no banco do back-end

---
+++ src/app/models/Order.ts
@@ -0,0 +1,31 @@
+import { model, Schema} from 'mongoose';
+
+export const Product = model('Category', new Schema({
+    table: {
+        type: String,
+        required: true,
+    },
+    status: {
+      type: String,
+      enum: ['WAITING', 'IN_PRODUCTION', 'DONE'],
+      default: 'WAITING',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    products: {
+        required: true,
+        type: [{
+            product:{
+                type: Schema.Types.ObjectId,
+                required: true,
+                ref: 'Category',
+            },
+            quantity: {
+                type: Number,
+                default: 1,
+            },
+        }],
+    },
+}))
