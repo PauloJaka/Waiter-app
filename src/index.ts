142d79830f346fda3e01f35d7f1b646fe2153ead
PauloJaka <zpaulogamer@gmail.com>
2023-07-29 23:26:58 -0300

    index app

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
-const app = express();
 
-app.listen(3002, () => {
-    console.log('emoji server is running on http://localhost:3002');
-})
+mongoose.connect('mongodb://localhost:27017')
+.then(() => {
+    const app = express();
+    app.listen(3002, () => {
+        console.log('emoji server is running on http://localhost:3002');
+    })
+} )
+.catch(() => console.log('erro ao conectar'));
+
+
