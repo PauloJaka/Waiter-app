21e78c2dedff562c78c3b1b94673f985abc83b4b
PauloJaka <zpaulogamer@gmail.com>
2023-07-30 15:02:20 -0300

    att: roots

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,31 @@
 import { Router } from 'express';
+import { listCategories } from './app/useCases/categories/listCategories';
 
 export const router = Router();
 
 
-router.get('/categories', (req,res) => {
+router.get('/categories', listCategories );
+
+router.post('/categories', (req,res) => {
+    res.send('OK');
+});
+
+router.post('/products', (req,res) => {
+    res.send('OK');
+});
+
+router.get('/categories/:categoryId/products', (req,res) => {
+    res.send('OK');
+});
+
+router.get('/orders', (req,res) => {
+    res.send('OK');
+});
+
+router.patch('/orders/:orderId', (req,res) => {
+    res.send('OK');
+});
+
+router.delete('/orders/:orderId', (req,res) => {
     res.send('OK');
 });
