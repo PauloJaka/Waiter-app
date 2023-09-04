7d5641986b90e30e344cf51c961fa365db8d66bb
PauloJaka <zpaulogamer@gmail.com>
2023-09-03 21:08:16 -0300

    listcategories and createCateogorys

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,23 @@
+import path from 'node:path';
 import { Router } from 'express';
 import { listCategories } from './app/useCases/categories/listCategories';
 import { createCategory } from './app/useCases/categories/createCategory';
 import { listProducts } from './app/useCases/products/listProducts';
-import { createProduct } from './app/useCases/products/createProduct';
+import { createProduct} from './app/useCases/products/createProduct';
+import multer from 'multer';
 
 export const router = Router();
 
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, callback){
+            callback(null, path.resolve(__dirname, '..', 'uploads'));
+        },
+        filename(req, file, callback){
+            callback(null, `${Date.now()}-${file.originalname}`);
+        },
+    }),
+});
 
 router.get('/categories', listCategories);
 
@@ -13,7 +25,7 @@ router.post('/categories', createCategory);
 
 router.get('/products', listProducts);
 
-router.post('/products', createProduct);
+router.post('/products',upload.single('image'),createProduct);
 
 router.post('/categories/:categoryId/products', (req,res) => {
     res.send('OK');
