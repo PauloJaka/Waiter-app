3db388eb1efd5ed09098160e459895bd133b44c4
PauloJaka <zpaulogamer@gmail.com>
2023-09-05 23:54:26 -0300

    att do dia

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { router } from "./router";
 
 mongoose.connect('mongodb://localhost:27017')
 .then(() => {
+
     const app = express();
 
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
@@ -14,9 +15,9 @@ mongoose.connect('mongodb://localhost:27017')
     app.use(router);
 
     app.listen(3002, () => {
-        console.log('emoji server is running on http://localhost:3002');
-    })
-} )
-.catch(() => console.log('erro ao conectar'));
+      console.log('emoji server is running on http://localhost:3002');
+    });
+  })
+  .catch(() => console.log('erro ao conectar'));
 
 
