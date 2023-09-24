9e11e3eb4133197c4749357a21a70530c8516f6a
PauloJaka <zpaulogamer@gmail.com>
2023-09-23 23:45:01 -0300

    orders icon and title

--- front-end/src/components/Orders/index.tsx
+++ front-end/src/components/Orders/index.tsx
@@ -4,9 +4,15 @@ import { OrdersBoard } from '../OrdersBoard';
 export function Orders() {
   return (
     <Container>
-      <OrdersBoard />
-      <OrdersBoard />
-      <OrdersBoard />
+      <OrdersBoard
+        icon = '🕒'
+        title =  'Fila de espera'/>
+      <OrdersBoard
+        icon = '👨‍🍳'
+        title = 'Em preparção'/>
+      <OrdersBoard
+        icon = '✅'
+        title = 'Pronto!'/>
     </Container>
   );
 }
