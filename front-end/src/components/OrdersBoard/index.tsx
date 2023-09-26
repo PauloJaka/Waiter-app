1991c426a879aae2379d28c07022b53b04c3efa2
PauloJaka <zpaulogamer@gmail.com>
2023-09-25 23:48:27 -0300

    use states in orderBoard with new functions

--- front-end/src/components/OrdersBoard/index.tsx
+++ front-end/src/components/OrdersBoard/index.tsx
@@ -1,6 +1,7 @@
-
 import { Order } from '../../types/Order';
+import { OrderModal } from '../OrderModal';
 import { Board, OrdersContainer } from './styles';
+import { useState } from 'react';
 
 interface OrderBoardProps{
   icon: string;
@@ -8,23 +9,31 @@ interface OrderBoardProps{
   orders: Order[];
 }
 
-export function OrdersBoard ({icon, title}: OrderBoardProps) {
+export function OrdersBoard ({icon, title, orders}: OrderBoardProps) {
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  function handleOpenModal () {
+    setIsModalVisible(true);
+  }
+
   return (
     <Board>
+      <OrderModal visible = {isModalVisible}/>
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
-        <span>(1)</span>
+        <span>({orders.length})</span>
       </header>
       <OrdersContainer>
-        <button>
-          <strong>Mesa 2</strong>
-          <span>2 itens</span>
-        </button>
-        <button>
-          <strong>Mesa 2</strong>
-          <span>2 itens</span>
-        </button>
+        {orders.length > 0 && (
+          orders.map((order) => (
+            <button key={order._id} onClick={handleOpenModal}>
+              <strong>Mesa {order.table}</strong>
+              <span>{order.products.length} itens</span>
+            </button>
+          ))
+        )}
       </OrdersContainer>
     </Board>
   );
